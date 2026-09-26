95098129411e6680ef774c4cf0339282d2b0a457
Dr. Hiteshkumar Agrawal <agrawal.hiteshkumar@gmail.com>
2026-09-26 12:49:18 +0530

    Update cohortTypes.ts

--- src/cohortTypes.ts
+++ src/cohortTypes.ts
@@ -1,1 +1,37 @@
+export interface AcademicClassCohort {
+  id: string;
+  name: string;
+  code: string;
+  program: 'D_PHARM' | 'B_PHARM';
+  academicYear: string;
+  termPattern: 'ANNUAL' | 'SEMESTER';
+  intakeCapacity: number;
+}
 
+export interface PracticalBatchDivision {
+  id: string;
+  classId: string;
+  batchName: string;
+  rollNumberRange: string;
+  assignedLabTeacherId: string;
+  assignedLabTeacherName: string;
+}
+
+export interface FacultyWorkloadAllocation {
+  id: string;
+  facultyId: string;
+  facultyName: string;
+  designation: string;
+  classId: string;
+  subjectCode: string;
+  subjectTitle: string;
+  weeklyTheoryHours: number;
+  weeklyPracticalHours: number;
+  totalWorkloadQuota: number;
+}
+
+export interface CohortCurricularMatrix {
+  classes: AcademicClassCohort[];
+  batches: PracticalBatchDivision[];
+  workloadAllocations: FacultyWorkloadAllocation[];
+}
